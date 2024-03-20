bfdc34726dab025204b9b7c7d78a8bccc9f099f2
kouassij <jacobcn21@gmail.com>
2024-03-20 16:34:31 +0100

    commit2

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,87 +3,86 @@ let compteur = 0; // Compteur qui permet de connaître l'image sur laquelle on s
 let timer, elements, slides, slideWidth, speed, transition;
 
 window.onload = () => {
-    // On récupère le diaporama
-    const diapo = document.querySelector(".diapo");
-    // On récupère le data-speed
-    speed = diapo.dataset.speed;
-    transition = diapo.dataset.transition;
+  // On récupère le diaporama
+  const diapo = document.querySelector(".diapo");
+  // On récupère le data-speed
+  speed = diapo.dataset.speed;
+  transition = diapo.dataset.transition;
 
-    elements = document.querySelector(".elements");
+  elements = document.querySelector(".elements");
 
-    // On clone la 1ère image
-    let firstImage = elements.firstElementChild.cloneNode(true);
+  // On clone la 1ère image
+  let firstImage = elements.firstElementChild.cloneNode(true);
 
-    // On injecte le clone à la fin du diapo
-    elements.appendChild(firstImage);
+  // On injecte le clone à la fin du diapo
+  elements.appendChild(firstImage);
 
-    slides = Array.from(elements.children);
+  slides = Array.from(elements.children);
 
-    // On récupère la largeur d'une slide
-    slideWidth = diapo.getBoundingClientRect().width;
+  // On récupère la largeur d'une slide
+  slideWidth = diapo.getBoundingClientRect().width;
 
-    // On récupère les flèches
-    let next = document.querySelector("#nav-droite");
-    let prev = document.querySelector("#nav-gauche");
+  // On récupère les flèches
+  let next = document.querySelector("#nav-droite");
+  let prev = document.querySelector("#nav-gauche");
 
-    // On gère le clic
-    next.addEventListener("click", slideNext);
-    prev.addEventListener("click", slidePrev);
+  // On gère le clic
+  next.addEventListener("click", slideNext);
+  prev.addEventListener("click", slidePrev);
 
-    // On automatise le défilement
-    timer = setInterval(slideNext, speed);
+  // On automatise le défilement
+  timer = setInterval(slideNext, speed);
 
-    // On gère l'arrêt et la reprise
-    diapo.addEventListener("mouseover", stopTimer);
-    diapo.addEventListener("mouseout", startTimer);
-}
+  // On gère l'arrêt et la reprise
+  diapo.addEventListener("mouseover", stopTimer);
+  diapo.addEventListener("mouseout", startTimer);
+};
 
 /**
  * Cette fonction fait défiler le diaporama vers la droite
  */
-function slideNext(){
-    // On incrémente le compteur
-    compteur++;
-    elements.style.transition = transition+"ms linear";
+function slideNext() {
+  // On incrémente le compteur
+  compteur++;
+  elements.style.transition = transition + "ms linear";
 
-    let decal = -slideWidth * compteur;
-    elements.style.transform = `translateX(${decal}px)`;
+  let decal = -slideWidth * compteur;
+  elements.style.transform = `translateX(${decal}px)`;
 
-    // On attend la fin de la transition et on "rembobine" de façon cachée
-    setTimeout(function(){
-        if(compteur >= slides.length - 1){
-            compteur = 0;
-            elements.style.transition = "unset";
-            elements.style.transform = "translateX(0)";
-        }
-    }, transition);
+  // On attend la fin de la transition et on "rembobine" de façon cachée
+  setTimeout(function () {
+    if (compteur >= slides.length - 1) {
+      compteur = 0;
+      elements.style.transition = "unset";
+      elements.style.transform = "translateX(0)";
+    }
+  }, transition);
 }
 
 /**
  * Cette fonction fait défiler le diaporama vers la gauche
  */
-function slidePrev(){
-    // On décrémente le compteur
-    compteur--;
-    elements.style.transition = transition+"ms linear";
+function slidePrev() {
+  // On décrémente le compteur
+  compteur--;
+  elements.style.transition = transition + "ms linear";
 
-    if(compteur < 0){
-        compteur = slides.length - 1;
-        let decal = -slideWidth * compteur;
-        elements.style.transition = "unset";
-        elements.style.transform = `translateX(${decal}px)`;
-        setTimeout(slidePrev, 1);
-    }
-
+  if (compteur < 0) {
+    compteur = slides.length - 1;
     let decal = -slideWidth * compteur;
+    elements.style.transition = "unset";
     elements.style.transform = `translateX(${decal}px)`;
-    
+    setTimeout(slidePrev, 1);
+  }
+
+  let decal = -slideWidth * compteur;
+  elements.style.transform = `translateX(${decal}px)`;
 }
 
-function stopTimer(){
-    clearInterval(timer);
+function stopTimer() {
+  clearInterval(timer);
 }
 
-function startTimer(){
-    timer = setInterval(slideNext, speed);
+function startTimer() {
+  timer = setInterval(slideNext, speed);
 }
